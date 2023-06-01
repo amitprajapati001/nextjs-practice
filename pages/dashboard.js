969393ba3bde22329075b47eecd5d54fd35f9d53
Amit <amitpr7828@gmail.com>
2023-06-01 23:17:59 +0530

    implemented scrool page

---
+++ pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Dashbaord from "../src/components/dashboard"
+
+const index=()=>{
+    return (
+         <Dashbaord />
+    )
+}
+
+export default Dashbaord;
---
+++ src/components/dashboard.js
@@ -0,0 +1,139 @@
+import React from "react";
+import Dashbaord from "../../pages/dashboard";
+
+function Dashboard(){
+     return(
+      <>
+      <div>sdhfdfjiodiofij</div>
+      <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+      <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+
+      <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div>
+
+        <div>sdhfdfjiodiofij</div>
+        <div className="container dashboard fluid row">
+            <div className="first "> <h1>First</h1>
+            <h1>First</h1>
+            <h1>First</h1><h1>First</h1>
+            </div>
+            <div className="second " >
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+
+        </div>
+        </div>
+        <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div>
+      <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+
+      <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div> <div>sdhfdfjiodiofij</div>
+       <div>sdhfdfjiodiofij</div>
+       <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1><h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <h1>Hello Elizabeth</h1>
+        <div>sdhfdfjiodiofij</div>
+        </>  
+     )
+}
+export default Dashboard;
